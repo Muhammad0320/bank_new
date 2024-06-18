580af9e868f528737c849758b8429931e1c7d479
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-18 00:32:33 -0700

    chore: update validators

--- transaction/src/service/validators.ts
+++ transaction/src/service/validators.ts
@@ -18,13 +18,15 @@ export const expiryMonthValidator = () =>
     .withMessage(' Month should be within 1 - 12 ');
 
 export const expiryYearValidator = () => {
-  const { getFullYear, getMonth } = new Date();
+  const  date = new Date();
+
+  // date.getFullYear();
 
   return check('expYear')
-    .isInt({ min: getFullYear(), max: getFullYear() + 5 })
+    .isInt({ min: date.getFullYear(), max: date.getFullYear() + 5 })
     .custom(
       (input: number, { req }) =>
-        input === getFullYear() && +req.expMonth - 1 >= getMonth()
+        input === date.getFullYear() && +req.expMonth - 1 >= date.getMonth()
     )
     .withMessage('Expired card!')
     .withMessage(' Invalid expiry year ');
