207cc970880d962bfe3158c9aaa051cabf498106
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-19 09:54:27 -0700

    chore:  update current user

--- common/src/middleware/currentUser.ts
+++ common/src/middleware/currentUser.ts
@@ -1,24 +1,29 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { UserStatus } from "../enums/UserStatus";
 import { UserRole } from "../enums/UserRoles";
 
 interface UserPayload {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-  role: UserRole;
-  avatar: string;
-  createdAt: Date;
-  status: UserStatus;
-  passwordConfirm: string;
+  user: {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    role: UserRole;
+    avatar: string;
+    createdAt: Date;
+    status: UserStatus;
+    passwordConfirm: string;
+  };
+
+  iat: Date;
+  exp: Date;
 }
 
 declare global {
   namespace Express {
     interface Request {
-      currentUser: UserPayload;
+      currentUser: UserPayload["user"];
     }
   }
 }
@@ -30,9 +35,11 @@ export const currentUser = (
 ) => {
   if (!req.session?.jwt) return next();
 
-  const user = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload;
+  const user = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as unknown;
 
-  req.currentUser = user;
+  const decoded = user as UserPayload;
+
+  req.currentUser = decoded.user;
 
   next();
 };
