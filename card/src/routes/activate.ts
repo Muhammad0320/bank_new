0931b99449a09da796f6e73ca91da38f48308b19
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-19 06:41:00 -0700

    chore:  update activate ts

--- card/src/routes/activate.ts
+++ card/src/routes/activate.ts
@@ -1,7 +1,12 @@
 import { Card } from '../model/card';
 import { CardStatus } from '../enums/CardStatus';
 import express, { Request, Response } from 'express';
-import { NotFound, paramsChecker, requireAuth } from '@m0banking/common';
+import {
+  BadRequest,
+  NotFound,
+  paramsChecker,
+  requireAuth
+} from '@m0banking/common';
 
 const router = express.Router();
 
@@ -13,6 +18,13 @@ router.patch(
     const card = await Card.findById(req.params.id);
 
     if (!card) throw new NotFound('Card not found');
+
+    const activatedCard = await card.updateOne(
+      { info: { status: CardStatus.Active } },
+      { new: true }
+    );
+
+    if (!activatedCard) throw new BadRequest('Something bad happened');
 
     res.status(200).json({ status: 'sucess', data: card });
   }
