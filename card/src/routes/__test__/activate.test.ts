170ed778cbe619b3709ae59d9f31fe456d0a4950
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-19 06:50:23 -0700

    chore:  updated my shit

--- card/src/routes/__test__/activate.test.ts
+++ card/src/routes/__test__/activate.test.ts
@@ -1,20 +1,20 @@
 import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
-import { CardNetwork } from '../../enums/CardNewtwork';
 import { CardType } from '../../enums/CardType';
 import { accountBuilder } from '../../test/builders';
+import { CardNetwork } from '../../enums/CardNewtwork';
 
 it(' returns a 401 for unauthenticated requests ', async () => {
   await request(app)
-    .patch('/shitid/activate')
+    .patch('/api/v1/card/shitid/activate')
     .send()
     .expect(401);
 });
 
 it('returns a 400 for invalid card ', async () => {
   await request(app)
-    .patch('/shitid/activate')
+    .patch('/api/v1/card/shitid/activate')
     .set('Cookie', await global.signin())
     .send()
     .expect(400);
@@ -24,7 +24,7 @@ it('retuns a 404 if the id does not match any existing card', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
 
   await request(app)
-    .patch(`/${id}/activate`)
+    .patch(`/api/v1/card/${id}/activate`)
     .set('Cookie', await global.signin())
     .send()
     .expect(404);
@@ -47,7 +47,7 @@ it('returns a 200 when everyting is valid ', async () => {
     .expect(201);
 
   await request(app)
-    .patch(`/${data.id}/activate`)
+    .patch(`/api/v1/card/${data.id}/activate`)
     .set('Cookie', await global.signin())
     .send()
     .expect(200);
