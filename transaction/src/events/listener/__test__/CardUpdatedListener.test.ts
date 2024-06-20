8f27d96c8a0d9d4be44e476f2ac5b5940096f46d
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-20 10:48:36 -0700

    chore:  updates a card settings

--- transaction/src/events/listener/__test__/CardUpdatedListener.test.ts
+++ transaction/src/events/listener/__test__/CardUpdatedListener.test.ts
@@ -3,6 +3,7 @@ import { natsWrapper } from '../../../natswrapper';
 import { CardBuilder } from '../../../test/builders';
 import { CardUpdatedListener } from '../CardUpdatedListener';
 import { Message } from 'node-nats-streaming';
+import { Card } from '../../../model/card';
 
 const setup = async () => {
   const listener = new CardUpdatedListener(natsWrapper.client);
@@ -27,3 +28,24 @@ const setup = async () => {
 
   return { msg, listener, data };
 };
+
+
+
+it("updates the card's settings ", async () => {
+  const { msg, listener, data } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedCard = await Card.findById(data.id);
+
+  if (!updatedCard) throw new Error('Card not found');
+
+  expect(updatedCard.settings.dailyLimit).toEqual(data.settings.dailyLimit);
+
+  expect(updatedCard.settings.weeklyLimit).toEqual(data.settings.weeklyLimit);
+
+  expect(updatedCard.settings.monthlyLimit).toEqual(data.settings.monthlyLimit);
+});
+
+
+  
