2684c4c2b4f87c8d8f3eb87d119541005642f3ff
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-20 09:08:33 -0700

    chore: complete card created listnner

---
+++ transaction/src/events/listener/CardCreatedListener.ts
@@ -0,0 +1,16 @@
+import { CardCreatedEvent, Listener, Subjects } from '@m0banking/common';
+import { queueGroupName } from './queueGroupName';
+import { Message } from 'node-nats-streaming';
+import { Card } from '../../model/card';
+
+export class CardCreatedListener extends Listener<CardCreatedEvent> {
+  readonly subject = Subjects.CardCreated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: CardCreatedEvent['data'], msg: Message) {
+    await Card.buildCard(data);
+
+    msg.ack();
+  }
+}
