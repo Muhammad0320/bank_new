96afed5cfb08933fe82ea1e2655f7666570595f8
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-20 09:15:17 -0700

    chore: add card activated listener

---
+++ transaction/src/events/listener/CardActivatedListener.ts
@@ -0,0 +1,27 @@
+import {
+  CardActivatedEvent,
+  CardStatus,
+  Listener,
+  Subjects
+} from '@m0banking/common';
+import { queueGroupName } from './queueGroupName';
+import { Message } from 'node-nats-streaming';
+import { Card } from '../../model/card';
+
+export class CardActivatedListener extends Listener<CardActivatedEvent> {
+  readonly subject = Subjects.CardActivated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: CardActivatedEvent['data'], msg: Message) {
+    const { id, version } = data;
+
+    const card = await Card.findByLastVersionAndId(id, version);
+
+    if (!card) throw new Error('Card not found');
+
+    await card.updateOne({ info: { status: CardStatus.Active } });
+
+    msg.ack();
+  }
+}
