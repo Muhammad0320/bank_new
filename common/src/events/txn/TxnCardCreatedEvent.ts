a7c8d9795ac16a3d74ad371ceb827f6971ef249a
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-20 22:52:18 -0700

    chore:  update txn card created event

---
+++ common/src/events/txn/TxnCardCreatedEvent.ts
@@ -0,0 +1,27 @@
+import { Subjects } from "../Subjects";
+
+export interface TxnCardCreatedEvent {
+  subject: Subjects.TxnCardCreated;
+
+  data: {
+    id: string;
+    version: number;
+    cardId: string;
+    amount: number;
+    reason?: string;
+
+    account: {
+      id: string;
+      userId: string;
+      version: number;
+      balance: number;
+    };
+
+    beneficiary: {
+      id: string;
+      userId: string;
+      version: number;
+      balance: number;
+    };
+  };
+}
