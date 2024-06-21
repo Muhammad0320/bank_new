c6b6e87215b40cdcb202a1d734a83cbeaea80284
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-21 09:46:36 -0700

    chore:  add the happy path

--- card/src/routes/__test__/all.test.ts
+++ card/src/routes/__test__/all.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
-import { UserRole } from '@m0banking/common';
+import { CardNetwork, CardType, UserRole } from '@m0banking/common';
+import { accountBuilder } from '../../test/builders';
 
 it(' returns a 401 for unauthrized acces ', async () => {
   await request(app)
@@ -18,4 +19,27 @@ it('returns a 403, if just ordinary user is not an admin ', async () => {
     .expect(403);
 });
 
-it('returns a 200 on for admins', async () => {});
+it('returns a 200 on for admins', async () => {
+  const account = await accountBuilder();
+
+  await request(app)
+    .post('/api/v1/card')
+    .set('Cookie', await global.signin(account.user.id))
+    .send({
+      accountId: account.id,
+      billingAddress: 'G50 Balogun gambari compd',
+      networkType: CardNetwork.Visa,
+      type: CardType.Credit
+    })
+    .expect(201);
+
+  const {
+    body: { data }
+  } = await request(app)
+    .get('/api/v1/card')
+    .set('Cookie', await global.signin(undefined, UserRole.Admin))
+    .send()
+    .expect(200);
+
+  expect(data.length).toEqual(1);
+});
