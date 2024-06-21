8e7d066ce70a47230defac3dbe9eef85d668c851
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-21 09:43:06 -0700

    chore:  update all test ts

--- card/src/routes/__test__/all.test.ts
+++ card/src/routes/__test__/all.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { UserRole } from '@m0banking/common';
 
 it(' returns a 401 for unauthrized acces ', async () => {
   await request(app)
@@ -7,3 +8,14 @@ it(' returns a 401 for unauthrized acces ', async () => {
     .send()
     .expect(401);
 });
+
+
+it('returns a 403, if just ordinary user is not an admin ', async () => {
+  await request(app)
+    .get('/api/v1/card')
+    .set('Cookie', await global.signin())
+    .send()
+    .expect(403);
+});
+
+it('returns a 200 on for admins', async () => {});
