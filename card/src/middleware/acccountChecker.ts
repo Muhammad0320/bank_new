a8445bf30b024105a69e0f0077ffbe03d166090d
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-20 22:31:10 -0700

    chore: update acount checker

--- card/src/middleware/acccountChecker.ts
+++ card/src/middleware/acccountChecker.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { Account } from '../model/Account';
 import { AccountStatus, BadRequest, NotFound } from '@m0banking/common';
+import { Card } from '../model/card';
 
 export const accountChecker = (type?: string) => async (
   req: Request,
@@ -11,7 +12,13 @@ export const accountChecker = (type?: string) => async (
 
   if (type === 'new') account = await Account.findById(req.body.accountId);
 
-  if (!type) account = await Account.findById(req.params.id);
+  if (!type) {
+    const card = await Card.findById(req.params.id).populate('account');
+
+    if (!card) throw new NotFound('Card not found');
+
+    account = card.account;
+  }
 
   if (!account) throw new NotFound('Account not found');
 
