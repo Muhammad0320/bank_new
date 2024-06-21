9ee6451b8634eb8f28cd62faa0ce7421aa968dc5
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-21 09:21:38 -0700

    chore:  update all test ts

--- card/src/routes/all.ts
+++ card/src/routes/all.ts
@@ -1,5 +1,18 @@
-import express from 'express';
+import { accessibleTo, requireAuth, UserRole } from '@m0banking/common';
+import express, { NextFunction, Request, Response } from 'express';
+import { Card } from '../model/card';
 
 const router = express.Router();
+
+router.get(
+  '/',
+  requireAuth,
+  accessibleTo(UserRole.Admin),
+  async (req: Request, res: Response) => {
+    const cards = await Card.find({});
+
+    res.status(200).json({ status: 'success', data: cards });
+  }
+);
 
 export { router as getAllCardRouter };
