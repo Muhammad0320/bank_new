00ddf3040702f46d089a400bdfd1529208f71824
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-21 09:32:36 -0700

    chore: update show ts

--- card/src/routes/show.ts
+++ card/src/routes/show.ts
@@ -1,5 +1,22 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { paramsChecker, requireAuth } from '@m0banking/common';
+import { accountChecker } from '../middleware/acccountChecker';
+import { Card } from '../model/card';
 
 const router = express.Router();
+
+router.get(
+  '/:id',
+  requireAuth,
+  paramsChecker('id'),
+  accountChecker(),
+  async (req: Request, res: Response) => {
+    const card = await Card.findById(req.params.id);
+
+    if (!card) return;
+
+    res.status(200).json({ status: 'success', data: card });
+  }
+);
 
 export { router as showCardRouter };
