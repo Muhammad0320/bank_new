32f756d0719cd40e45b9acf7b5788bed27450335
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-21 10:01:14 -0700

    chore:  add a route handler that updates a show test ts

--- card/src/routes/__test__/show.test.ts
+++ card/src/routes/__test__/show.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import mongoose from 'mongoose';
 
 it('returns a 401, for invalid request', async () => {
   await request(app)
@@ -11,6 +12,16 @@ it('returns a 401, for invalid request', async () => {
 it('returns a 400 for invalid mongoose id', async () => {
   await request(app)
     .get('/api/v1/card/shitid')
+    .set('Cookie', await global.signin())
+    .send()
+    .expect(400);
+});
+
+
+
+it('returns a 400 if the card id provided is valid but does not match any in the db', async () => {
+  await request(app)
+    .get('/api/v1/card/' + new mongoose.Types.ObjectId().toHexString())
     .set('Cookie', await global.signin())
     .send()
     .expect(400);
