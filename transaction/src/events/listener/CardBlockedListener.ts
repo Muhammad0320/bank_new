976d68ba17f2d1f7116e39d358042ea89e2301b1
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-21 20:26:22 -0700

    chore: update card bloked lidtener

--- transaction/src/events/listener/CardBlockedListener.ts
+++ transaction/src/events/listener/CardBlockedListener.ts
@@ -1,5 +1,5 @@
 import {
-  CardBlockedEent,
+  CardBlockedEvent,
   CardStatus,
   Listener,
   Subjects
@@ -8,12 +8,12 @@ import { queueGroupName } from './queueGroupName';
 import { Message } from 'node-nats-streaming';
 import { Card } from '../../model/card';
 
-export class CardBlockedListener extends Listener<CardBlockedEent> {
+export class CardBlockedListener extends Listener<CardBlockedEvent> {
   readonly subject = Subjects.CardBlocked;
 
   queueGroupName = queueGroupName;
 
-  async onMessage(data: CardBlockedEent['data'], msg: Message) {
+  async onMessage(data: CardBlockedEvent['data'], msg: Message) {
     const { id, version } = data;
 
     const card = await Card.findByLastVersionAndId(id, version);
