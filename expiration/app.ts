2d91575ebe935a096bd2464797f4d9eb2440ad16
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-14 18:24:06 +0100

    chore:  update skaffold yaml

---
+++ expiration/app.ts
@@ -0,0 +1,47 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+
+import { currentUser, globalErrorHandler, NotFound } from '@m0banking/common';
+import { createTxnRouter } from './routes/newDeposit';
+import { createTransferRouter } from './routes/newTransfer';
+import { createWithdrawalRouter } from './routes/newWithdrawal';
+import { invalidAttemptTracker } from './middlewares/invalidAttemptTracker';
+import { cardTxn } from './routes/cardTxn';
+
+const app = express();
+
+app.set('trust proxy', true);
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    httpOnly: true,
+    signed: false,
+    secure: false
+  })
+);
+
+console.log('Hi mom');
+
+const rootUrl = '/api/v1/txn';
+
+app.use(currentUser);
+
+// app.use(invalidAttemptTracker);
+
+app.use(rootUrl, cardTxn);
+app.use(rootUrl, createTxnRouter);
+app.use(rootUrl, createTransferRouter);
+app.use(rootUrl, createWithdrawalRouter);
+
+app.all('*', () => {
+  throw new NotFound('Route not found');
+});
+
+app.use(globalErrorHandler);
+
+export { app };
---
+++ expiration/index.ts
@@ -0,0 +1,68 @@
+import { app } from './app';
+import mongoose from 'mongoose';
+import { natsWrapper } from './natswrapper';
+import { AccountCreatedListener } from './events/listener/AccountCreatedListener';
+import { AccountBlockedListener } from './events/listener/AccountBlockedListener';
+import { AccountUnblockedListener } from './events/listener/AccountUnBlockedListener';
+import { CardCreatedListener } from './events/listener/CardCreatedListener';
+import { CardUpdatedListener } from './events/listener/CardUpdatedListener';
+import { CardBlockedListener } from './events/listener/CardBlockedListener';
+import { CardActivatedListener } from './events/listener/CardActivatedListener';
+
+const start = async () => {
+  const port = 3000;
+
+  if (!process.env.JWT_KEY) {
+    throw new Error(' JWT_KEY not found ');
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error(' MONGO_URI not found ');
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+
+  try {
+        await natsWrapper.connect(
+          process.env.NATS_CLUSTER_ID,
+          process.env.NATS_CLIENT_ID,
+          process.env.NATS_URL
+        );
+
+        natsWrapper.client.on('close', () => {
+          console.log(' NATS connection closed! ');
+          process.exit();
+        });
+
+        process.on('SIGTERM', () => natsWrapper.client.close());
+        process.on('SIGINT', () => natsWrapper.client.close());
+
+        new AccountCreatedListener(natsWrapper.client).listen();
+        new AccountBlockedListener(natsWrapper.client).listen();
+        new AccountUnblockedListener(natsWrapper.client).listen();
+        new CardCreatedListener(natsWrapper.client).listen();
+        new CardUpdatedListener(natsWrapper.client).listen();
+        new CardActivatedListener(natsWrapper.client).listen();
+        new CardBlockedListener(natsWrapper.client).listen();
+
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log('connected to mongoDB');
+      } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(port, () => {
+    console.log('App running on port ', port);
+  });
+};
+
+start();
