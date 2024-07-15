dd4ab9002e75f197dd86b7929d8f5752b6f2994e
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-15 18:26:14 +0100

    chore:  update card expiration isten

--- card/src/events/listener/__test__/cardExpiredListener.test.ts
+++ card/src/events/listener/__test__/cardExpiredListener.test.ts
@@ -1,7 +1,9 @@
-import { CardExpirationEvent } from '@m0banking/common';
+import { CardExpirationEvent, CardStatus } from '@m0banking/common';
 import { cardBuilder } from '../../../test/builders';
 import { CardExpiredListener } from '../CardExpiredListener';
 import { natsWrapper } from '../../../natswrapper';
+import { Message } from 'node-nats-streaming';
+import { Card } from '../../../model/card';
 
 const setup = async () => {
   const card = await cardBuilder();
@@ -14,9 +16,19 @@ const setup = async () => {
 
   // @ts-ignore
 
-  const msg = {
+  const msg: Message = {
     ack: jest.fn()
   };
 
   return { card, data, msg, listener };
 };
+
+it('updates and saves card status', async () => {
+  const { card, data, msg, listener } = await setup();
+
+  listener.onMessage(data, msg);
+
+  const refetchedCard = await Card.findById(card.id);
+
+  expect(refetchedCard?.info.status).toEqual(CardStatus.Expired);
+});
