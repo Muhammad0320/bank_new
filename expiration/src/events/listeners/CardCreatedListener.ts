7576b2590abaa9621524305afa8f54c0df768fdf
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-15 17:53:31 +0100

    chore:  update card created listener

--- expiration/src/events/listeners/CardCreatedListener.ts
+++ expiration/src/events/listeners/CardCreatedListener.ts
@@ -1,1 +1,19 @@
-export class CardCreatedListener {}
+import { CardCreatedEvent, Listener, Subjects } from '@m0banking/common';
+import { queueGroupName } from './queueGroupName';
+import { Message } from 'node-nats-streaming';
+import { cardExpirationQueue } from '../../queue/expirationQueue';
+
+export class CardCreatedListener extends Listener<CardCreatedEvent> {
+  queueGroupName = queueGroupName;
+
+  readonly subject = Subjects.CardCreated;
+
+  async onMessage(data: CardCreatedEvent['data'], msg: Message) {
+    const delay =
+      new Date(data.info.expiryDate).getTime() - new Date().getTime();
+
+    await cardExpirationQueue.add({ cardId: data.id }, { delay });
+
+    msg.ack();
+  }
+}
