16e24dc6787263ea8abb1db6a6d641f2ae1bbcee
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-15 18:15:54 +0100

    chore :  update card expired listener

--- card/src/events/listener/CardExpiredListener.ts
+++ card/src/events/listener/CardExpiredListener.ts
@@ -1,11 +1,25 @@
-import { Listener, CardExpirationEvent, Subjects } from '@m0banking/common';
+import {
+  Listener,
+  CardExpirationEvent,
+  Subjects,
+  CardStatus
+} from '@m0banking/common';
 import { queueGroupName } from './queueGroupName';
 import { Message } from 'node-nats-streaming';
+import { Card } from '../../model/card';
 
 export class CardExpiredListener extends Listener<CardExpirationEvent> {
   readonly subject = Subjects.CardExpired;
 
   queueGroupName = queueGroupName;
 
-  async onMessage(data: CardExpirationEvent['data'], msg: Message) {}
+  async onMessage(data: CardExpirationEvent['data'], msg: Message) {
+    const card = await Card.findById(data.cardId);
+
+    if (!card) throw new Error('Card not found');
+
+    await card.updateOne({ info: { status: CardStatus.Expired } });
+
+    msg.ack();
+  }
 }
