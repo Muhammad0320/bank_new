10c731d797a202b308bb8aa6c3f495f312f639f9
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-16 05:01:10 +0100

    chore:  update index ts

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,19 +1,7 @@
-import { app } from './app';
-import mongoose from 'mongoose';
 import { natsWrapper } from './natswrapper';
 import { CardCreatedListener } from './events/listeners/CardCreatedListener';
 
 const start = async () => {
-  const port = 3000;
-
-  if (!process.env.JWT_KEY) {
-    throw new Error(' JWT_KEY not found ');
-  }
-
-  if (!process.env.MONGO_URI) {
-    throw new Error(' MONGO_URI not found ');
-  }
-
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error('NATS_CLIENT_ID must be defined');
   }
@@ -25,32 +13,24 @@ const start = async () => {
   }
 
   try {
-        await natsWrapper.connect(
-          process.env.NATS_CLUSTER_ID,
-          process.env.NATS_CLIENT_ID,
-          process.env.NATS_URL
-        );
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
 
-        natsWrapper.client.on('close', () => {
-          console.log(' NATS connection closed! ');
-          process.exit();
-        });
+    natsWrapper.client.on('close', () => {
+      console.log(' NATS connection closed! ');
+      process.exit();
+    });
 
-        process.on('SIGTERM', () => natsWrapper.client.close());
-        process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+    process.on('SIGINT', () => natsWrapper.client.close());
 
-        new CardCreatedListener(natsWrapper.client).listen();
-
-        await mongoose.connect(process.env.MONGO_URI);
-
-        console.log('connected to mongoDB');
-      } catch (error) {
+    new CardCreatedListener(natsWrapper.client).listen();
+  } catch (error) {
     console.error(error);
   }
-
-  app.listen(port, () => {
-    console.log('App running on port ', port);
-  });
 };
 
 start();
