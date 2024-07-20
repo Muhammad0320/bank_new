ee07428252873f325d800c877c091a11ef4bb6e2
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-20 18:07:22 +0100

    chore:  update signin ts

--- user/src/routes/signinWithUsername.ts
+++ user/src/routes/signinWithUsername.ts
@@ -1,5 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { passwordValidator, usernameValidator } from '../services/validators';
+import { requestValidator } from '@m0banking/common';
 
 const router = express.Router();
+
+router.post(
+  '/signin/username',
+  [usernameValidator(), passwordValidator()],
+  requestValidator,
+  async (req: Request, res: Response) => {}
+);
 
 export { router as signinWithUsernameRouter };
