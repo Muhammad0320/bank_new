fb4df53b229dc8983dd7366915119fd81b5e1ef0
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-20 18:21:07 +0100

    chore:  update signin user ts

--- user/src/routes/signinWithUsername.ts
+++ user/src/routes/signinWithUsername.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import { passwordValidator, usernameValidator } from '../services/validators';
-import { requestValidator } from '@m0banking/common';
+import { BadRequest, CryptoManager, requestValidator } from '@m0banking/common';
+import User from '../model/user';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -8,7 +10,34 @@ router.post(
   '/signin/username',
   [usernameValidator(), passwordValidator()],
   requestValidator,
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+
+    const existingUSer = await User.findOne({ username }).select('+password');
+
+    if (!!!existingUSer) throw new BadRequest('Invalid signin credentials');
+
+    const isCorrectPassword = await CryptoManager.compare(
+      existingUSer.password,
+      password
+    );
+
+    if (!isCorrectPassword) throw new BadRequest('Invalid signin credentials');
+
+    const token = jwt.sign({ user: existingUSer }, process.env.JWT_KEY!, {
+      expiresIn: +process.env.JWT_EXPIRES_IN! * 60 * 60
+    });
+
+    req.session = {
+      jwt: token
+    };
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Signin succssful',
+      data: existingUSer
+    });
+  }
 );
 
 export { router as signinWithUsernameRouter };
