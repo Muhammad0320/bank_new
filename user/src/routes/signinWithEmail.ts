fd642731441f54fdd6db605a9b2f9fc15a4a727f
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-20 17:54:27 +0100

    chore:  update signin with email ts

---
+++ user/src/routes/signinWithEmail.ts
@@ -0,0 +1,56 @@
+import jwt from 'jsonwebtoken';
+
+import express, { Request, Response } from 'express';
+import {
+  BadRequest,
+  CryptoManager,
+  emailValidator,
+  passwordValidator,
+  requestValidator
+} from '@m0banking/common';
+import User from '../model/user';
+
+const router = express.Router();
+
+router.post(
+  '/signin/email',
+
+  [emailValidator(), passwordValidator()],
+
+  requestValidator,
+
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email }).select('+password');
+
+    if (!existingUser) {
+      throw new BadRequest('Invalid login credentials email ');
+    }
+
+    const isCorrectPassword = await CryptoManager.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!isCorrectPassword) {
+      throw new BadRequest('Invalid login credentials password ');
+    }
+
+    const token = jwt.sign({ user: existingUser }, process.env.JWT_KEY!, {
+      expiresIn: +process.env.JWT_EXPIRES_IN! * 60 * 60
+    });
+
+    req.session = {
+      jwt: token
+    };
+
+    await existingUser.updateOne({
+      signinTimeStamps: [...existingUser.signinTimeStamps, new Date()]
+    });
+
+    res.status(200).json({ status: 'success', data: existingUser });
+  }
+);
+
+export { router as signinWithEmailRouter };
