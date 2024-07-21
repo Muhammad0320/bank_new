318fcdf252874606443d954d29d6993a8d25f775
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-21 18:20:43 +0100

    chore:  update signin with usernam

--- client/services/signinWithUsername.ts
+++ client/services/signinWithUsername.ts
@@ -1,3 +1,23 @@
-import { SigninWUsernameSchema } from '../type/User';
+import { SigninWUsernameSchema, User } from '../type/User';
+import { rootUrl } from '../utils/variable';
 
-export const signinWithUsername = async (formData: SigninWUsernameSchema) => {};
+export const signinWithUsername = async (formData: SigninWUsernameSchema) => {
+  const res = await fetch(rootUrl + '/user/signin/username', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+
+    body: JSON.stringify(formData),
+  });
+
+  if (!res.ok) {
+    console.log(res);
+
+    throw new Error('somrthing hsppend while signining in');
+  }
+
+  const data = (await res.json()) as User;
+
+  return data;
+};
