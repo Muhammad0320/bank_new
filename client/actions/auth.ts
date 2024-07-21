f5896cb79188239d03065524a40f890f275c8004
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-21 17:58:56 +0100

    chore:  update auth ts

--- client/actions/auth.ts
+++ client/actions/auth.ts
@@ -1,53 +1,9 @@
 'use server';
 
-import z from 'zod';
-import { UserStatus } from '../utils/enums';
-import { UserUpdatesObj } from '../utils/types';
-import { redirect } from 'next/navigation';
-import { revalidatePath, revalidateTag } from 'next/cache';
-import { formDataConverter } from '../utils/formatter';
-import { signupApi } from '../services/signup';
 import { fromZodError } from 'zod-validation-error';
-
-const PasswordType = z.object({
-  password: z.string().min(8, 'Password too short'),
-});
-
- export const SigninWithEmailFormSchema = z
-   .object({
-     email: z.string().email({ message: 'Provide a valid email' }),
-   })
-   .merge(PasswordType);
-
- export const SigninWithUsernameSchema = z
-   .object({
-     username: z.string().min(6, 'Invalid username format: min of 6 chars'),
-   })
-   .merge(PasswordType);
-
- const UserSchema = z.object({
-   name: z.string({ message: 'Invalid name format' }),
-   email: z.string().email('Invalid email format'),
-   phone: z.number().min(13, 'Invalid phone format'),
-   password: z.string().min(8, 'Password too short'),
-   passwordConfirm: z.string(),
-   username: z.string().min(6, 'Invalid username format: min of 6 chars'),
- });
-
- export type FormSchema = z.infer<typeof UserSchema>;
- export type SigninWEmailSchema = z.infer<typeof SigninWithEmailFormSchema>;
- export type SigninWUsernameSchema = z.infer<typeof SigninWithUsernameSchema>;
-
-
-export type User = FormSchema & {
-  id: string;
-  version: number;
-  createdAt: Date;
-  status: UserStatus;
-  avatar?: string;
-  signinTimeStamps: Date[];
-  updates: UserUpdatesObj[];
-};
+import { signupApi } from '../services/signup';
+import { FormSchema, UserSchema } from '../type/User';
+import { formDataConverter } from '../utils/formatter';
 
 export const signupAction = async (prevState: any, formData: FormData) => {
   const parsedData = formDataConverter<FormSchema>(formData);
@@ -68,5 +24,3 @@ export const signupAction = async (prevState: any, formData: FormData) => {
   // revalidateTag('signup');
   // redirect('/');
 };
-
-
