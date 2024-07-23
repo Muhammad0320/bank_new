e2b23aab1e0a8cf190235d2c50b3971593983d8d
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-23 18:35:52 +0100

    chore:  update signup ts

--- client/services/signup.ts
+++ client/services/signup.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
+import https from 'https';
 import { FormSchema, User } from '../type/User';
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  httpsAgent: new https.Agent({ rejectUnauthorized: false }),
 });
 
 export const signupApi: (formData: FormSchema) => Promise<User> = async (
