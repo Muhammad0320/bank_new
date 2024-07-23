2180943717a78d7f4f61c6a652970e6efd7e1552
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-23 18:56:20 +0100

    chore:  update code

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,6 +2,15 @@
 
 const nextConfig = {
   /* config options here */
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // Capture any API requests
+        destination: 'https://banking.dev/api/v1/:path*', // Forward to backend URL
+      },
+    ];
+  },
 };
 
 export default nextConfig;
