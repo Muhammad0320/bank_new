2c3edac6ce53528ef63e23f1cd8df28a1030b9b6
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-24 17:48:02 +0100

    chore: update signup ts

--- client/services/signup.ts
+++ client/services/signup.ts
@@ -9,9 +9,9 @@ const axiosInstance = axios.create({
 
 export const signupApi = async (formData: FormSchema) => {
   try {
-    // const res = await axiosInstance.get('/user');
-    const res = await fetch('https://banking.com/api/v1/user');
-    const data = res;
+    const res = await axiosInstance.get('/user');
+
+    const data = res.data;
     console.log(data, 'from the signup api');
     return data;
   } catch (error) {
