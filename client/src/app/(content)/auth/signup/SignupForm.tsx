c3aeeae57846d3559682092dfcbd1687c98a542b
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-25 17:41:13 +0100

    çhore:  update signup from

---
+++ client/src/app/(content)/auth/signup/SignupForm.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useFormState } from 'react-dom';
+import { signupAction } from '../../../../../actions/auth';
+import Input from '../../../../../components/UI/Input';
+import Button from '../../../../../components/UI/Button';
+import { FormEvent } from 'react';
+import axios from 'axios';
+import { axiosInstance } from '../../../../../lib/axios';
+
+const FormPage = () => {
+  const initialState: any = { message: [] };
+
+  const [state, formAction] = useFormState(signupAction, initialState);
+
+  const handleSignup = async (e: FormEvent) => {
+    e.preventDefault();
+
+    // try {
+    //   const res = await ('/api/v1/user');
+
+    //   console.log(res.data);
+    // } catch (error) {
+    //   console.log('Foem Error:', error);
+    // }
+  };
+
+  return (
+    <div>
+      <form action={formAction}>
+        <Input id="name" label="name" type="string" />
+        <Input id="email" label="email" type="email" />
+        <Input id="password" label=" password" type="password" />
+        <Input id="passwordConfirm" label="password confirm" type="password" />
+        <Input id="phone" label="phone" type="number" />
+        <Input id="username" label="username" type="text" />
+
+        <Button pending="Submitting"> Submit </Button>
+      </form>
+    </div>
+  );
+};
+
+export default FormPage;
