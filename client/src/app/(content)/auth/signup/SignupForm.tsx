b110991120e381f861c32ad4773475e1928e4fc3
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-26 06:43:00 +0100

    chore:  uodate signuo form

--- client/src/app/(content)/auth/signup/SignupForm.tsx
+++ client/src/app/(content)/auth/signup/SignupForm.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { FC } from 'react';
+import { FC, FormEvent } from 'react';
 import { useFormState } from 'react-dom';
 import Input from '../../../../../components/UI/Input';
 import Button from '../../../../../components/UI/Button';
+import axios from 'axios';
 
 const FormPage: FC<{
   action: (prevState: any, formData: FormData) => Promise<{ errors: {}[] }>;
@@ -12,9 +13,34 @@ const FormPage: FC<{
 
   console.log(state, 'The error state from code');
 
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const res = await axios.get('https://banking.com/api/v1/user');
+
+    const data = res.data;
+
+    console.log(data);
+  };
+
+  // return (
+  //   <div>
+  //     <form action={formAction}>
+  //       <Input id="name" label="name" type="string" />
+  //       <Input id="email" label="email" type="email" />
+  //       <Input id="password" label=" password" type="password" />
+  //       <Input id="passwordConfirm" label="password confirm" type="password" />
+  //       <Input id="phone" label="phone" type="number" />
+  //       <Input id="username" label="username" type="text" />
+
+  //       <Button pending="Submitting"> Submit </Button>
+  //     </form>
+  //   </div>
+  // );
+
   return (
     <div>
-      <form action={formAction}>
+      <form onSubmit={handleSubmit}>
         <Input id="name" label="name" type="string" />
         <Input id="email" label="email" type="email" />
         <Input id="password" label=" password" type="password" />
