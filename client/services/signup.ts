b3a4e90e4e73dd44e2d64a5458c602072506a4d5
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-26 06:20:15 +0100

    chore:  uodate code

--- client/services/signup.ts
+++ client/services/signup.ts
@@ -1,28 +1,14 @@
-'use server';
-
 import axios from 'axios';
 import { FormSchema } from '../type/User';
-import { formDataConverter } from '../utils/formatter';
 
-export const signupApi: (
-  prevState: any,
-  formData: FormData
-) => Promise<{ errors: {}[] }> = async (prevState: any, formData: FormData) => {
+export const signupApi = async (formData: FormSchema) => {
   try {
-    const formDatas = formDataConverter(formData);
-
     // const res = await axiosInstance.get('/user');
 
-    const errors = [{}];
-
-    const res = await axios.get('/api/v1/user');
+    const res = await axios.get('https://banking.com/api/v1/user');
 
     const data = res.data;
     console.log(data, 'from the signup api');
-
-    return {
-      errors,
-    };
   } catch (error) {
     console.error('Fetch error:', error);
     throw error;
